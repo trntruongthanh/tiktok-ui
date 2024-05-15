3bf773086a584fe2c34687385aeafbdd7236246f
trntruongthanh <trntruongthanh@gmail.com>
2024-05-15 15:32:54 +0700

    Build the default Layout frame

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';           // npm i classnames
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);                 // để có thể sử dụng className={cx('post-item)}
+
 function Header() {
-    return <h2>Header</h2>
+  return <header className={cx('wrapper')}>
+    <div className={cx('inner')}>
+    </div>
+  </header>;
 }
 
-export default Header; 
+export default Header;
