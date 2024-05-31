abe84fd84d7ec2af05f7a96bfce9d7bcd29d6908
trntruongthanh <trntruongthanh@gmail.com>
2024-05-31 09:07:56 +0700

    UI Header part-5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -31,6 +31,19 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAfrica} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          code: 'en',
+          title: 'English',
+        },
+        {
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faQuestionCircle} />,
@@ -51,6 +64,11 @@ function Header() {
       setSearchResult([]);
     }, 0);
   }, []);
+
+
+  const handleMenuChange = (MenuItem) => {
+    console.log(MenuItem);
+  }
 
   return (
     <header className={cx('wrapper')}>
@@ -91,7 +109,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
